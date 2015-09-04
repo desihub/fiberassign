8fce2abbd197043ed01be0a9c19d9ff5551ec925
rncahn <rncahn@lbl.gov>
2015-09-04 14:46:37 -0700

    move printf in structs.cpp

--- src/structs.cpp
+++ src/structs.cpp
@@ -128,9 +128,10 @@ Gals read_galaxies_ascii(const Feat& F)
         Q.z = redshift;
         Q.ra = ra;
         Q.dec = dec;
-        if(oid%500000==0)printf( "%d  %8.4f   %8.4f   %8.4f %8.4f   %8.4f   %8.4f \n",Q.id, Q.z, Q.ra, Q.dec, Q.nhat[0],Q.nhat[1],Q.nhat[2]);
+
         if (oid%F.moduloGal == 0) {
         try{P.push_back(Q);}catch(std::exception& e) {myexception(e);}
+        if(oid%500000==0)printf( "%d  %8.4f   %8.4f   %8.4f %8.4f   %8.4f   %8.4f \n",Q.id, Q.z, Q.ra, Q.dec, Q.nhat[0],Q.nhat[1],Q.nhat[2]);
         }
         oid++;
         getline(fs,buf);
