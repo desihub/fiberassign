c7b60d36f75b38d2893d40f23f5755ed43244ec8
rncahn <rncahn@lbl.gov>
2015-09-04 15:33:53 -0700

    add \n to printf in Assignment

--- src/structs.cpp
+++ src/structs.cpp
@@ -297,7 +297,7 @@ List av_gals_of_kind(int kind, int j, int k, const Gals& G, const Plates& P, con
 
 // Assignment -----------------------------------------------------------------------------
 Assignment::Assignment(const Gals& G, const Feat& F) {
-    printf("start Assignment");
+    printf("start Assignment\n");
 	TF = initTable(F.Nplate,F.Nfiber,-1);//galaxy assigned to tile-fiber TF[j][k]
 	GL = initPtable(F.Ngal,0); //tile-fiber pair for galaxy  GL[g]
 	order.resize(F.Nplate);
@@ -308,14 +308,14 @@ Assignment::Assignment(const Gals& G, const Feat& F) {
 	nobsv = initList(F.Ngal);//remaining observations
 	nobsv_tmp = initList(F.Ngal);//remaining observations before we know truth
 	List l = F.maxgoal();//max observations for a category
-    printf("set up plates");
+    printf("set up plates\n");
 	for (int g=0; g<F.Ngal; g++) {
 		nobsv[g] = F.goal[G[g].id];//true goal
 		nobsv_tmp[g] = l[G[g].id];//max goal for category
 	}
-    printf("set up galaxies");
+    printf("set up galaxies\n");
 	unused = initTable(F.Nplate,F.Npetal,F.Nfbp);//initialized to number of fibers on a petal
-    printf("set up unused");
+    printf("set up unused\n");
 }
 
 Assignment::~Assignment() {}
