4705234f41bdb61c3107326c57f9ba6cdae6eff7
rncahn <rncahn@lbl.gov>
2015-09-19 17:49:04 -0700

    check prio in feat

--- src/feat.cpp
+++ src/feat.cpp
@@ -97,7 +97,11 @@ void Feat::readInputFile(const char file[]) {
 				init_types();
 				init_ids_types();
 			}
-			if (tok[0]=="prio") for (int i=0; i<Categories; i++) prio.push_back(s2d(tok[i+1]));
+            if (tok[0]=="prio") {
+                for (int i=0; i<Categories; i++) prio.push_back(s2d(tok[i+1]));
+                for (int i=0; i<Categories; i++) printf("  %f ",prio[i]);
+                printf(" \n");
+            }
 			if (tok[0]=="priopost") for (int i=0; i<Categories; i++) priopost.push_back(s2d(tok[i+1]));
 			if (tok[0]=="goal") for (int i=0; i<Categories; i++) goal.push_back(s2i(tok[i+1]));
             if (tok[0]=="goalpost") for (int i=0; i<Categories; i++) goalpost.push_back(s2i(tok[i+1]));
