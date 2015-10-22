6a4873d49e10607119758ff655fb2b09d295c6af
rncahn <rncahn@lbl.gov>
2015-10-21 18:00:53 -0700

    diagnostic in new_replace

--- src/global.cpp
+++ src/global.cpp
@@ -111,22 +111,24 @@ inline int find_best(int j, int k, const MTL& M, const Plates& P, const PP& pp, 
 	// For all available galaxies
 	for (int gg=0; gg<av_gals.size(); gg++) {
 		int g = av_gals[gg];
-		int m = M[g].nobs_remain; // Check whether it needs further observation
-        if (m>=1) {
-            int prio = M[g].t_priority;
+        if(!(M[g].t_priority==9800&&P[j].SF_in_petal[pp.spectrom[k]]>MaxSF-1)&&!(M[g].t_priority==9900&&P[j].SF_in_petal[pp.spectrom[k]]>MaxSS-1)){
+            int m = M[g].nobs_remain; // Check whether it needs further observation
+            if (m>=1) {
+                int prio = M[g].t_priority;
             
-			// Takes it if better priority, or if same, if it needs more observations, so shares observations if two QSOs are close
-			if (prio<pbest || (prio==pbest && m>mbest)) {
+                // Takes it if better priority, or if same, if it needs more observations, so shares observations if two QSOs are close
+                if (prio<pbest || (prio==pbest && m>mbest)) {
 
-				// Check that g is not assigned yet on this plate, or on the InterPlate around, check with ok_to_assign
-                int isa=A.is_assigned_jg(j,g,M,F);
-                int ok=ok_assign_g_to_jk(g,j,k,P,M,pp,F,A);
-                if (isa==-1 && ok && g!=no_g ) {
-					best = g;
-					pbest = prio;
-					mbest = m;
-				}
-			}
+                    // Check that g is not assigned yet on this plate, or on the InterPlate around, check with ok_to_assign
+                    int isa=A.is_assigned_jg(j,g,M,F);
+                    int ok=ok_assign_g_to_jk(g,j,k,P,M,pp,F,A);
+                    if (isa==-1 && ok && g!=no_g ) {
+                        best = g;
+                        pbest = prio;
+                        mbest = m;
+                    }
+                }
+            }
         }
 	}
 	return best;
@@ -169,6 +171,7 @@ inline void assign_galaxy(int g,  MTL& M, Plates& P, const PP& pp, const Feat& F
 // Tries to assign (j,k) to a SS (preferentially because they have priority) or a SF
 // no limit on the number of times a SS or SF can be observed
 // not used 10/20/15
+/*
 inline int assign_fiber_to_ss_sf(int j, int k, MTL& M, Plates& P, const PP& pp, const Feat& F, Assignment& A) {
 	int best = -1; int pbest = 10000;
 	List av_gals = P[j].av_gals[k];
@@ -186,7 +189,7 @@ inline int assign_fiber_to_ss_sf(int j, int k, MTL& M, Plates& P, const PP& pp, 
 	if (best!=-1) A.assign(j,k,best,M,P,pp);
 	return best;
 }
-
+*/
 // Takes an unassigned fiber and tries to assign it with the "improve" technique described in the doc
 // not used for SS or SF   because it is used before we add SS and SF
 inline int improve_fiber(int begin, int next, int j, int k, MTL& M, Plates& P, const PP& pp, const Feat& F, Assignment& A, int no_g=-1) {
