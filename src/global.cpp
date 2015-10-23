b4b00c3431f82325504496abcc31b54b324dbab8
rncahn <rncahn@lbl.gov>
2015-10-22 21:40:43 -0700

    require SS not be previously assigned this plate

--- src/global.cpp
+++ src/global.cpp
@@ -350,7 +350,7 @@ void new_replace( int j, int p, MTL& M, Plates& P, const PP& pp, const Feat& F, 
             }
         }
     }
-        if(j%1000==0){printf(" j= %d p=%d after  SS_in_petal  %d   SF_in_petal   %d\n ", j,p, P[j].SS_in_petal[p],P[j].SF_in_petal[p]);}
+    if(j%1000==0){printf(" j= %d p=%d after  SS_in_petal  %d   SF_in_petal   %d\n ", j,p, P[j].SS_in_petal[p],P[j].SF_in_petal[p]);}
     for(int c=M.priority_list.size()-3;P[j].SF_in_petal[p]<F.MaxSF && c>-1;--c ){//try to do this for lowest priority
         // aside from SS and SF, so size()-3
         std::vector <int> gals=P[j].SF_av_gal[p]; //standard stars on this plate
@@ -360,6 +360,7 @@ void new_replace( int j, int p, MTL& M, Plates& P, const PP& pp, const Feat& F, 
             Plist tfs=M[g].av_tfs;
             int done=0;
             for(int i;i<tfs.size() && done==0;++i){
+                printf(" i %d  tfs.f %d  tfb.s %d\",i,tfs[i].f,tfs[i].s);
                 if(tfs[i].f==j){
                     int k=tfs[i].s;//we know g can be reached by this petal of plate j and fiber k
                     int g_old=A.TF[j][k];//what is now at (j,k)
