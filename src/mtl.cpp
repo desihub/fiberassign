672754fe340b0d9ce75c988bf574c931de471ad9
rncahn <rncahn@lbl.gov>
2015-10-27 12:42:08 -0700

    shuffle files

---
+++ src/mtl.cpp
@@ -0,0 +1,50 @@
+#include	<cstdlib>
+#include	<cmath>
+#include	<fstream>
+#include	<sstream>
+#include	<iostream>
+#include	<iomanip>
+#include	<string>
+#include	<vector>
+#include	<algorithm>
+#include	<exception>
+#include	<sys/time.h>
+#include	"modules/htmTree.h"
+#include	"modules/kdTree.h"
+#include        "misc.h"
+#include        "feat.h"
+#include        "structs.h"
+#include        "collision.h"
+#include        "global.h"
+//reduce redistributes, updates  07/02/15 rnc
+int main(int argc, char **argv) {
+	//// Initializations ---------------------------------------------
+	srand48(1234); // Make sure we have reproducability
+	check_args(argc);
+	Time t, time; // t for global, time for local
+	init_time(t);
+	Feat F;
+
+	// Read parameters file //
+	F.readInputFile(argv[1]);
+	printFile(argv[1]);
+	// Read galaxies
+	Gals G;
+    if(F.Ascii){
+        G=read_galaxies_ascii(F);}
+    else{
+        G = read_galaxies(F);
+    }
+	F.Ngal = G.size();
+	printf("# Read %s galaxies from %s \n",f(F.Ngal).c_str(),F.galFile.c_str());
+    std::vector<int> count;
+    count=count_galaxies(G);
+    printf(" Number of galaxies by type, QSO-Ly-a, QSO-tracers, LRG, ELG, fake QSO, fake LRG, SS, SF\n");
+    for(int i=0;i<8;i++){printf (" type %d number  %d  \n",i, count[i]);}
+    // make MTL
+    MTL Min=make_MTL(G,F);
+    write_MTLfile(Min);
+
+	return(0);
+  
+}
