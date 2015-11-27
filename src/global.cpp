1f08afd343ea77507923dbac54019269888ce7c7
rncahn <rncahn@lbl.gov>
2015-11-27 05:59:32 -0800

    clean up

--- src/global.cpp
+++ src/global.cpp
@@ -447,21 +447,16 @@ void assign_sf_ss(int j, MTL& M, Plates& P, const PP& pp, const Feat& F, Assignm
     }
 }
 
-//void redistribute_tf(MTL& M, Plates&P, const PP& pp, const Feat& F, Assignment& A, int next) {
 void redistribute_tf(MTL& M, Plates&P, const PP& pp, const Feat& F, Assignment& A) {
 	Time t;
 	init_time(t,"# Begin redistribute TF :");
 	int j0 = A.next_plate;//among only plates with galaxies
 	//int n = next==-1 ? F.Nplate-A.next_plate : next; //from next_plate on
     int n = F.Nplate-A.next_plate;
-	//List randPlates = F.Randomize ? random_permut(plates) : plates;
-	//List randPlates = random_permut(plates);
 	int red(0);
 	Table Done = initTable(F.Nplate,F.Nfiber);//consider every occupied plate and every fiber
 	for (int j=j0; j<F.Nplate; j++) {
-		//int j = randPlates[jj];
         int js=A.suborder[j];
-        printf(" j = %d  js = %d \n",j,js);
 		List randFiber = random_permut(F.Nfiber);
 		for (int kk=0; kk<F.Nfiber; kk++) {
 			int k = randFiber[kk];
