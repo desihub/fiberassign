b6cb6110182a24ac6fdc3b9f7829705e842d7490
rncahn <rncahn@lbl.gov>
2015-11-30 16:49:55 -0800

    fix new_replace

--- src/global.cpp
+++ src/global.cpp
@@ -348,11 +348,10 @@ void new_replace( int j, int p, MTL& M, Plates& P, const PP& pp, const Feat& F, 
                 int done=0;//quit after we've used this SS
                 for(int i=0;i<tfs.size() && done==0;++i){
                     if(tfs[i].f==j&&pp.spectrom[tfs[i].s]==p){//a tile fiber from this petal
+                        int k=tfs[i].s;//we know g can be reached by this petal of plate j and fiber k
+                        int g_old=A.TF[j][k];//what is now at (j,k)  g_old can't be -1 or we would have used it already in assign_sf
                         if(g_old!=-1 && !M[g].SS && !M[g].SF){
-                            int k=tfs[i].s;//we know g can be reached by this petal of plate j and fiber k
-                            int g_old=A.TF[j][k];//what is now at (j,k)  g_old can't be -1 or we would have used it already in assign_sf
                             if (M[g_old].priority_class==c&&A.is_assigned_jg(j,g,M,F)==-1 && ok_for_limit_SS_SF(g,j,k,M,P,pp,F)){
-
                                 //right priority; this SS not already assigned on this plate
                                 A.unassign(j,k,g_old,M,P,pp);
                                 assign_galaxy(g_old,M,P,pp,F,A);//try to assign
