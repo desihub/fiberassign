ea5bf05c95b1989e24f8e73d503b5fc109aa2ce7
rncahn <rncahn@lbl.gov>
2015-12-02 14:54:16 -0800

    diagnosstic in new_replace

--- src/global.cpp
+++ src/global.cpp
@@ -386,6 +386,7 @@ void new_replace( int j, int p, MTL& M, Plates& P, const PP& pp, const Feat& F, 
                         int g_old=A.TF[j][k];//what is now at (j,k)
                         if(g_old!=-1 && !M[g_old].SS && !M[g_old].SF){
                             if (M[g_old].priority_class==c&&A.is_assigned_jg(j,g,M,F)==-1 && ok_for_limit_SS_SF(g,j,k,M,P,pp,F)){
+                                printf(" j %d k %d   g %d  g_old  %d \n ");
                                 A.unassign(j,k,g_old,M,P,pp);
                                 assign_galaxy(g_old,M,P,pp,F,A);//try to assign
                                 A.assign(j,k,g,M,P,pp);
