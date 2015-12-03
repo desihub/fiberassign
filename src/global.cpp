7bcbcf7869de36747a631f5cebb75d9d2d9f0aa3
rncahn <rncahn@lbl.gov>
2015-12-02 16:11:41 -0800

    trest unassign

--- src/global.cpp
+++ src/global.cpp
@@ -387,7 +387,9 @@ void new_replace( int j, int p, MTL& M, Plates& P, const PP& pp, const Feat& F, 
                         if(g_old!=-1 && !M[g_old].SS && !M[g_old].SF){
                             if (M[g_old].priority_class==c&&A.is_assigned_jg(j,g,M,F)==-1 && ok_for_limit_SS_SF(g,j,k,M,P,pp,F)){
                                 printf("SF j %d k %d   g %d  g_old  %d \n ",j,k,g,g_old);
+                                printf(" before unassign occupant of (j,k)  %d\n",A.TF[j][k]);
                                 A.unassign(j,k,g_old,M,P,pp);
+                                printf(" after unassign occupant of (j,k)  %d\n",A.TF[j][k]);
                                 assign_galaxy(g_old,M,P,pp,F,A);//try to assign
                                 A.assign(j,k,g,M,P,pp);
                                 done=1;
