b5ed73b57e06f549662ec1f99713063a729f1f54
rncahn <rncahn@lbl.gov>
2015-12-04 15:05:59 -0800

    diagnostic

--- src/fa.cpp
+++ src/fa.cpp
@@ -171,6 +171,7 @@ int main(int argc, char **argv) {
 
 	//Execute plan, updating targets at intervals
     std::vector <int> update_intervals=F.pass_intervals;
+    printf("before push back\n");
     update_intervals.push_back(F.NUsedplate);//to end intervals at last plate
     printf("made update_intervals\n");
     for(int i=0;i<update_intervals.size()-1;++i){//go plate by used plate
