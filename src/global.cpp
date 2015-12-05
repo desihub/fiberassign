2919a0382cfbfaf0635249e6827ed431916f10f7
rncahn <rncahn@lbl.gov>
2015-12-05 09:28:45 -0800

    drop improve

--- src/global.cpp
+++ src/global.cpp
@@ -196,8 +196,8 @@ inline int improve_fiber(int begin, int j, int k, MTL& M, Plates& P, const PP& p
 			// For all available galaxies within reach that are already observed
 			for (int i=0; i<av_g.size(); i++) {
 				int g = av_g[i];//a galaxy accessible to js,k
-                if (g!=-1 && g!=no_g&&!M[g].SS&!M[g].SF) {//not SS or SF
-					if (ok_assign_g_to_jk(g,js,k,P,M,pp,F,A)&&ok_for_limit_SS_SF(g,j,k,M,P,pp,F)) {
+                if (g!=-1 && g!=no_g && !M[g].SS && !M[g].SF) {//not SS or SF
+					if (ok_assign_g_to_jk(g,js,k,P,M,pp,F,A) && ok_for_limit_SS_SF(g,j,k,M,P,pp,F)) {
                         // Which tile-fibers have taken g ?
 						Plist tfs = A.chosen_tfs(g,F,A.suborder[begin]);//all tile-fibers that observe g in tiles from begin to end
                         for (int p=0; p<tfs.size(); p++) {
