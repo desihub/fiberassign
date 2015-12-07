9846053ec37ac5f1f74c8297a2c723fc8773430c
rncahn <rncahn@lbl.gov>
2015-12-07 15:48:12 -0800

    remove order

--- src/structs.cpp
+++ src/structs.cpp
@@ -555,7 +555,7 @@ Assignment::Assignment(const MTL& M, const Feat& F) {
 	GL = initPtable(F.Ngal,0); //tile-fiber pair for galaxy  GL[g]
     inv_order=initList(F.Nplate,-1);
     
-    for (int i=0; i<F.Nplate; i++) order[i] = i;
+    //for (int i=0; i<F.Nplate; i++) order[i] = i;
 	next_plate = 0;
 	kinds = initCube(F.Nplate,F.Npetal,F.Categories);
 	unused = initTable(F.Nplate,F.Npetal,F.Nfbp);//initialized to number of fibers on a petal
