a6b0906468ab6f4786d5c4ccc13277a926e76d23
rncahn <rncahn@lbl.gov>
2015-12-29 13:50:37 -0800

    more diagnostics

--- src/global.cpp
+++ src/global.cpp
@@ -508,6 +508,7 @@ void redistribute_tf(MTL& M, Plates&P, const PP& pp, const Feat& F, Assignment& 
                         std::cout.flush();
                         
 						int unused = A.unused[jp][pp.spectrom[kp]];//unused for jp, spectrom[kp]
+                        printf("unused %d\n",unused);
                         if(A.inv_order[jp]>F.NUsedplate || A.inv_order[jp]<0)printf("**out range  %d\n",A.inv_order[jp]);
                         if (A.suborder[jused_start]<=jp){
                             if(!A.is_assigned_tf(jp,kp)){
