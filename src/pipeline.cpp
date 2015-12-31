063591ab764ad6f2cec655902b1edc3ab185ca9f
rncahn <rncahn@lbl.gov>
2015-12-30 17:23:50 -0800

    jused only in ascii write

--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -185,7 +185,7 @@ int main(int argc, char **argv) {
             write_FAtile_ascii(j,F.outDir,M,P,pp,F,A);
         }
 
-    if (F.PrintFits) for (int j=0; j<F.Nplate; j++){
+    if (F.PrintFits) for (int j=0; j<F.Nplate; jused++){
         fa_write(j,F.outDir,M,P,pp,F,A); // Write output
     }
     /*
