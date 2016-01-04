c7e004766d125d62cb5878928fb02886c527db35
rncahn <rncahn@lbl.gov>
2016-01-04 09:55:55 -0800

    improve diagnostic

--- src/mtl.cpp
+++ src/mtl.cpp
@@ -47,7 +47,7 @@ int main(int argc, char **argv) {
     count=count_galaxies(G);
     printf(" Number of galaxies by type, QSO-Ly-a, QSO-tracers, LRG, ELG, fake QSO, fake LRG, SS, SF\n");
     std::cout.flush();
-    for(int i=0;i<2;i++){
+    for(int i=0;i<count.size();i++){
         printf (" type %d number  %d  \n",i, count[i]);
         std::cout.flush();
     }
