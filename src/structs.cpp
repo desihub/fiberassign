83bca28eaf4657ade2f66b5ffe79e224ce044b68
rncahn <rncahn@lbl.gov>
2016-01-04 05:05:22 -0800

    diagnostic

--- src/structs.cpp
+++ src/structs.cpp
@@ -82,6 +82,7 @@ Gals read_galaxies(const Feat& F) {
 std::vector<int> count_galaxies(const Gals& G){
     std::vector <int> counter(10,0);
     for (int i=0;i<G.size();i++){
+        printf("i  %d G[i] %d\n ",i,G[i]);
         if(G[i].id<0){
             printf("id less than zero= %d\n",G[i].id);
             std::cout.flush();}
