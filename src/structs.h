1cb2d9e929eb8a8c3d0c19c72c8d693fbb53147f
rncahn <rncahn@lbl.gov>
2016-01-04 11:21:39 -0800

    fix targ stuff

--- src/structs.h
+++ src/structs.h
@@ -68,9 +68,10 @@ class MTL : public std::vector<struct target> {
 void make_MTL(const Gals& G, const Feat& F, Gals& Secret, MTL& M);
 void make_MTL_SS_SF(const Gals& G, MTL& Targ, MTL& SStars, MTL& SkyF,Gals& Secret, const Feat& F);
 MTL read_MTLfile(str filename, const Feat& F, int SS, int SF);
-
+void make_Targ_Secret(const Gals& G, MTL& Targ, Gals& Secret, const Feat& F);
 void write_MTLfile(const Gals& Secret, const MTL& M, const Feat& F);
-void write_MTL_SS_SFfile(const MTL& Targ, const MTL& SStarts, const MTL& SkyF, const Gals& Secret, const Feat& F);
+void write_MTL_SS_SFfile(const MTL& Targ, const MTL& SStars, const MTL& SkyF, const Gals& Secret, const Feat& F);
+void write_Targ_Secret(const MTL& Targ,  const Gals& Secret, const Feat& F);
 void assign_priority_class(MTL & M);
 // petal--------------------------------------------------
 
