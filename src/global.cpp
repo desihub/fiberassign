910e8f382fbe0785d12eedf8d256a9f428576182
rncahn <rncahn@lbl.gov>
2016-01-04 18:59:44 -0800

    more disagnostic

--- src/global.cpp
+++ src/global.cpp
@@ -277,7 +277,8 @@ void update_plan_from_one_obs(int jused,const Gals& Secret, MTL& M, Plates&P, co
 	List to_update;	// Get the list of galaxies to update in the plan
 	for (int k=0; k<F.Nfiber; k++) {
         int g = A.TF[j][k];
-
+        printf(" g %d  M[g].SS  %d  M[g].SF  %d  Secret[g].id %d\n", g, M[g].SS, M[g].SF ,Secret[g].id );
+        std::cout.flush();
         if (g!=-1&&!M[g].SS && !M[g].SF){        // Don't update SS or SF
             //initially nobs_remain==goal
             if(M[g].once_obs==0){//first observation  otherwise should be ok
