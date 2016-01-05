741ceaca31b64ca485c381d86470b14638fe7c99
rncahn <rncahn@lbl.gov>
2016-01-04 16:39:46 -0800

    diagnostic

--- src/fa.cpp
+++ src/fa.cpp
@@ -179,11 +179,17 @@ int main(int argc, char **argv) {
         printf(" beginning at %d\n",starter);
         std::cout.flush();
         for (int jused=starter; jused<update_intervals[i+1]; jused++) {
+            printf(" jused %d\n",jused);
+            std::cout.flush();
+
             if (0<=jused-F.Analysis) {
                 update_plan_from_one_obs(jused,Secret,M,P,pp,F,A);
-            }else printf("\n no update\n");
-            // Update corrects all future occurrences of wrong QSOs etc and tries to observe something else
                 printf(" jused %d\n",jused);
+                std::cout.flush();
+            }
+            else printf("\n no update\n");
+            // Update corrects all future occurrences of wrong QSOs etc and tries to observe something else
+
         }
         redistribute_tf(M,P,pp,F,A,starter);
         improve(M,P,pp,F,A,starter);
