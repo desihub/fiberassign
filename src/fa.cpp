4ddce27add9c57f2b784d910ec7a6d0a69ac1592
rncahn <rncahn@lbl.gov>
2016-01-05 04:39:46 -0800

    diagnostic

--- src/fa.cpp
+++ src/fa.cpp
@@ -113,6 +113,7 @@ int main(int argc, char **argv) {
     Assignment A(M,F);
     
 	print_time(t,"# Start assignment at : ");
+    sstd::cout.flush();
 
 	// Make a plan ----------------------------------------------------
     // Plans whole survey without sky fibers, standard stars
