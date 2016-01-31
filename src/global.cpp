45567168f2f934f9c7472d452f87649b9cb206a8
rncahn <rncahn@lbl.gov>
2016-01-30 16:12:23 -0800

    remove diagnostics

--- src/global.cpp
+++ src/global.cpp
@@ -701,7 +701,6 @@ void display_results(str outdir, const Gals& Secret,const MTL& M, const Plates& 
         std::cout.flush();
 		for (int k=0; k<F.Nfiber; k++) {
 			int g = A.TF[j][k];
-            printf("k %d  g %d\n",k,g);
             std::cout.flush();
             if (g!=-1) galaxs[g]++;
 		}
@@ -710,7 +709,7 @@ void display_results(str outdir, const Gals& Secret,const MTL& M, const Plates& 
 			for (int g=0; g<F.Ntarg; g++) {
 				int n = galaxs[g];
                 
-                if(n>0)printf("g  %d n  %d   sec  %d\n",g,n,Secret[g].id);
+                //if(n>0)printf("g  %d n  %d   sec  %d\n",g,n,Secret[g].id);
                 std::cout.flush();
 
 				if (1<=n) {
