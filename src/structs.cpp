f77765d425544b8e5b4c4dce0a9d2981e5352aa9
rncahn <rncahn@lbl.gov>
2016-02-02 10:40:42 -0800

    fix z in targ_secret

--- src/structs.cpp
+++ src/structs.cpp
@@ -312,7 +312,7 @@ void write_Targ_Secret(const MTL& Targ, const Gals& Secret, const Feat& F){
     str sd=F.Secretfile;
     FD = fopen(sd.c_str(),"w");
     for (int i=0;i<Secret.size();++i){
-        fprintf(FD," %d Secret %f  %f  %d   \n",      i,Secret[i].ra,Secret[i].dec,Secret[i].id);
+        fprintf(FD," %d Secret %f  %f  %f %d   \n",      i,Secret[i].ra,Secret[i].dec,Secret[i].z,Secret[i].id);
     }
     fclose(FD);
     
