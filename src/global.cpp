341693f54b84e178855d4e6dac956694cf0b6365
rncahn <rncahn@lbl.gov>
2016-02-16 13:49:05 -0800

    suppress diagnostic

--- src/global.cpp
+++ src/global.cpp
@@ -271,7 +271,7 @@ void update_plan_from_one_obs(int jused,const Gals& Secret, MTL& M, Plates&P, co
 	if (jpast<0) { printf("ERROR in update : jpast negative\n"); fl(); }
     int j=A.suborder[jpast];
     //diagnostic
-    printf("j %d  jused %d \n",j,jused);
+    //printf("j %d  jused %d \n",j,jused);
     std::cout.flush();
 	//int na_start(A.na(F,j0,n));//unassigned fibers in tiles from j0 to j0+n
 	List to_update;	// Get the list of galaxies to update in the plan
