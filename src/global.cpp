6b793a619b8c44eef174d31151b02fffe6d19e21
rncahn <rncahn@lbl.gov>
2016-03-07 14:16:31 -0800

    add c.str to outdir

--- src/global.cpp
+++ src/global.cpp
@@ -824,7 +824,7 @@ void display_results(str outdir, const Gals& Secret,const MTL& M, const Plates& 
 
 void write_FAtile_ascii(int j, str outdir, const MTL& M, const Plates& P, const PP& pp, const Feat& F, const Assignment& A) {
     FILE * FA;
-    str s = outdir.c_str+"tile"+i2s(j)+".txt";
+    str s = outdir.c_str()+"tile"+i2s(j)+".txt";
     FA = fopen(s.c_str(),"w");
     for (int k=0; k<F.Nfiber; k++) {
         int g = A.TF[j][k];
