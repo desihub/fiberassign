08701fb2b3b4704e579bc30b01a9eb8d3680b3be
rncahn <rncahn@lbl.gov>
2016-04-28 16:37:42 -0700

    deal with negative RAs

--- bin/full_map_any_sky.py
+++ bin/full_map_any_sky.py
@@ -58,7 +58,7 @@ while (1==1):
     if (ra_min>=0 & ra_max>=0):
         ii=(mra<ra_max)&(mra>ra_min)&(mdec<dec_max)&(mdec>dec_min)
     if (ra_min<0 & ra_max>=0):
-        split=(mra<ra_max) or (mra>ra_min+360)
+        split=any([(mra<ra_max) , (mra>ra_min+360)])
         ii=(mdec<dec_max)&(mdec>dec_min)&split
     if (ra_min<0 & ra_max<=0):
         ii=(mra<360+ra_max)&(mra>360+ra_min)&(mdec<dec_max)&(mdec>dec_min)
