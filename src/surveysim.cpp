fdd0476ceac2e0d47f6446e816940bff1f3dac27
rncahn <rncahn@lbl.gov>
2016-05-02 13:47:45 -0700

    count galaxies in first pass

--- src/surveysim.cpp
+++ src/surveysim.cpp
@@ -121,7 +121,7 @@ int main(int argc, char **argv) {
         Plist v=M[g].av_tfs;
         int done=0;
         for(int i=0;i<v.size()&&done==0;++i){
-            if(ipass[v[i].f]==0){
+            if(P[v[i].f].ipass==0){
                 ++outside;
                 done=1;
             }
