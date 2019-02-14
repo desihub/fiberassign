1f65aa54f048feedb39c83e6538227eb849f3dae
Theodore Kisner <tskisner.public@gmail.com>
2019-02-14 13:15:26 -0800

    We are actually installing the package in order to import it and generate the docs.  Add our requirements for pip and remove the mocks.

--- doc/conf.py
+++ doc/conf.py
@@ -19,8 +19,6 @@ import sys
 import os
 import os.path
 
-from importlib import import_module
-
 # This is a hybrid package with a compiled extension.  The extension must be
 # built before running sphinx.  First, we simply try to import fiberassign,
 # which will pick up either the installed version or one that has been built
@@ -140,17 +138,6 @@ keep_warnings = True
 
 # Include functions that begin with an underscore, e.g. _private().
 napoleon_include_private_with_doc = True
-
-# This value contains a list of modules to be mocked up. This is useful when
-# some external dependencies are not met at build time and break the
-# building process.
-autodoc_mock_imports = []
-for missing in ('numpy', 'scipy', 'matplotlib', 'fitsio', 'astropy',
-                'desimodel', 'desitarget', 'desiutil'):
-    try:
-        foo = import_module(missing)
-    except ImportError:
-        autodoc_mock_imports.append(missing)
 
 # -- Options for HTML output ----------------------------------------------
 
