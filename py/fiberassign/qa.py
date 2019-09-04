a553b4c81afb9eb53dc4356d8be2f879e11b8f06
Sarah Eftekharzadeh <s.eftekharzadeh@gmail.com>
2019-09-03 22:15:08 -0600

    removed the residuals of a different test

--- py/fiberassign/qa.py
+++ py/fiberassign/qa.py
@@ -45,7 +45,7 @@ def qa_parse_table(header, tgdata):
         lrgmask = int(desi_mask["LRG"].mask)
         elgmask = int(desi_mask["ELG"].mask)
         qsomask = int(desi_mask["QSO"].mask)
-        badmask = [int(desi_mask["BAD_SKY"].mask),int(desi_mask["SUPP_SKY"].mask)]
+        badmask = int(desi_mask["BAD_SKY"].mask)
         
         for row in range(len(tgdata)):
             tgid = tgdata["TARGETID"][row]
@@ -85,7 +85,6 @@ def qa_tile(hw, tile_id, tgs, tgprops, tile_assign, tile_avail):
     unassigned = list()
     objtypes = dict()
     
-    print(hw.device_locations('POS'))
     for lid in locs:
         if lid not in tile_assign:
             unassigned.append(int(lid))
