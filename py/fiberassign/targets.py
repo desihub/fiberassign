72902963fe734a366a9d2d0d255553f068b6b6b8
Andrew Cooper <a.p.cooper@durham.ac.uk>
2020-05-12 21:44:29 -0700

    Remove redundant ORs with mws scnd bits
    When creating sv1_sciencemask in targets.py, all targets with SCND_ANY are
    included. This makes the subsequent ORs with individual MWS secondary bits
    redundant.

--- py/fiberassign/targets.py
+++ py/fiberassign/targets.py
@@ -125,51 +125,8 @@ def default_sv1_sciencemask():
 
     if "SCND_ANY" in desi_mask.names():
         sciencemask |= desi_mask["SCND_ANY"].mask
-        sciencemask |= sv1_scnd_mask['MWS_WD_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_DRACO_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_DDOGIANTS_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_WDBINARY_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_RRLYR_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_HYADES_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_GC_SV']
-        sciencemask |= sv1_scnd_mask['MWS_WD_SV_VERYBRIGHT']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_DDOGIANTS_VERYBRIGHT_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_HYADES_VERYBRIGHT_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_GC_VERYBRIGHT_SV']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_APOGEE']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_APOGEE_VERYBRIGHT']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_GAIAESO']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_GAIAESO_VERYBRIGHT']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_SEGUE']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_SEGUE_VERYBRIGHT']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_GALAH']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_GALAH_VERYBRIGHT']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_BOSS']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_BOSS_VERYBRIGHT']
-
     else:
         sciencemask |= desi_mask["SECONDARY_ANY"].mask
-        sciencemask |= sv1_scnd_mask['MWS_WD_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_DRACO_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_DDOGIANTS_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_WDBINARY_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_RRLYR_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_HYADES_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_GC_SV']
-        sciencemask |= sv1_scnd_mask['MWS_WD_SV_VERYBRIGHT']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_DDOGIANTS_VERYBRIGHT_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_HYADES_VERYBRIGHT_SV']
-        sciencemask |= sv1_scnd_mask['MWS_SPECIAL_GC_VERYBRIGHT_SV']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_APOGEE']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_APOGEE_VERYBRIGHT']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_GAIAESO']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_GAIAESO_VERYBRIGHT']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_SEGUE']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_SEGUE_VERYBRIGHT']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_GALAH']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_GALAH_VERYBRIGHT']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_BOSS']
-        sciencemask |= sv1_scnd_mask['MWS_CALIB_BOSS_VERYBRIGHT']
 
     return sciencemask
 
