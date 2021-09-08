3b3efa01b9ea28388423b68923bdf3d3e1d6076c
Dustin Lang <dstndstn@gmail.com>
2021-09-08 09:39:23 -0700

    bunch of special cases for SV tiles

--- bin/patch-fa.py
+++ bin/patch-fa.py
@@ -17,9 +17,11 @@ def read_secondary(fn):
         return _sec_cache[fn]
     except KeyError:
         pass
-    f = fitsio.read(fn)
-    _sec_cache[fn] = f
-    return f
+    scnd = fitsio.read(fn)
+    sidmap = dict([(tid,i) for i,tid in enumerate(scnd['TARGETID']) if tid > 0])
+    val = scnd,sidmap
+    _sec_cache[fn] = val
+    return val
 
 def patch(infn = 'desi/target/fiberassign/tiles/trunk/001/fiberassign-001000.fits.gz',
           desiroot = '/global/cfs/cdirs/desi',
@@ -51,18 +53,50 @@ def patch(infn = 'desi/target/fiberassign/tiles/trunk/001/fiberassign-001000.fit
     #tilera  = primhdr['TILERA']
     #tiledec = primhdr['TILEDEC']
     tileid = primhdr['TILEID']
+    fa_surv = primhdr['FA_SURV']
 
     # Find targeting files / directories in the headers.
     #mtldir = primhdr['MTL']
-    targdir = primhdr['TARG']
-    scndfn = primhdr.get('SCND', '')
+    targdirs = [primhdr['TARG']]
+    for i in range(2, 100):
+        key = 'TARG%i' % i
+        if not key in primhdr:
+            break
+        targdirs.append(primhdr[key])
+    scndfn = primhdr.get('SCND', '').strip()
     toofn = primhdr.get('TOO', '')
 
     # Swap in the local file locations. (Header values may start with the string "DESIROOT")
     #mtldir = mtldir.replace('DESIROOT', desiroot)
-    targdir = targdir.replace('DESIROOT', desiroot)
+    targdirs = [targdir.replace('DESIROOT', desiroot)
+                for targdir in targdirs]
     scndfn = scndfn.replace('DESIROOT', desiroot)
     toofn = toofn.replace('DESIROOT', desiroot)
+
+    # Tile 80611 has /data as the prefix.
+    pre = '/data/'
+    targdirs = [targdir.replace(pre, desiroot+'/')
+                if targdir.startswith(pre)
+                else targdir
+                for targdir in targdirs]
+    if scndfn.startswith(pre):
+        scndfn = scndfn.replace(pre, desiroot+'/')
+    if toofn.startswith(pre):
+        toofn = toofn.replace(pre, desiroot+'/')
+
+    # Tile 80687 has SCND a directory, not a file.
+    if os.path.isdir(scndfn):
+        # Tile 80687 has SCND a directory that contains two files, with the same number of rows
+        # but one includes DR9 photometry columns.
+        fn = os.path.join(scndfn, 'sv1targets-dark-secondary-dr9photometry.fits')
+        if os.path.exists(fn):
+            print('Special-case updated SCND from', scndfn, 'to', fn)
+            scndfn = fn
+        # Similar with 80717.
+        fn = os.path.join(scndfn, 'sv1targets-bright-secondary-dr9photometry.fits')
+        if os.path.exists(fn):
+            print('Special-case updated SCND from', scndfn, 'to', fn)
+            scndfn = fn
 
     # Read the original FIBERASSIGN table.
     hduname = 'FIBERASSIGN'
@@ -93,21 +127,27 @@ def patch(infn = 'desi/target/fiberassign/tiles/trunk/001/fiberassign-001000.fit
     alltargets = []
     tidset = set(targetid[targetid > 0])
     for hp in np.unique(hps):
-        pat = os.path.join(targdir, '*-hp-%i.fits' % hp)
-        #print(pat)
-        fns = glob(pat)
-        #print(fns)
-        if len(fns) != 1:
-            print('Searching for pattern', pat, 'found files', fns, '; expected 1 file.')
-        assert(len(fns) == 1)
-        fn = fns[0]
-        T = fitsio.read(fn)
-        tid = T['TARGETID']
-        I = np.flatnonzero([t in tidset for t in tid])
-        print('Read targets', fn, '->', len(T), 'targets,', len(I), 'matching (positive) TARGETIDs')
-        if len(I) == 0:
-            continue
-        alltargets.append(T[I])    
+        foundhp = False
+        for targdir in targdirs:
+            pat = os.path.join(targdir, '*-hp-%i.fits' % hp)
+            #print(pat)
+            fns = glob(pat)
+            #print(fns)
+            if len(fns) != 1:
+                print('Searching for pattern', pat, 'found files', fns, '; expected 1 file.')
+            assert(len(fns) <= 1)
+            if len(fns) == 0:
+                continue
+            foundhp = True
+            fn = fns[0]
+            T = fitsio.read(fn)
+            tid = T['TARGETID']
+            I = np.flatnonzero([t in tidset for t in tid])
+            print('Read targets', fn, '->', len(T), 'targets,', len(I), 'matching (positive) TARGETIDs')
+            if len(I) == 0:
+                continue
+            alltargets.append(T[I])
+        assert(foundhp)
 
     # Merge targets and match on TARGETID.
     targets = np.hstack(alltargets)
@@ -145,10 +185,13 @@ def patch(infn = 'desi/target/fiberassign/tiles/trunk/001/fiberassign-001000.fit
             patched_rows.update(J[diff])
 
     # Were secondary targets used for this tile?
+    if scndfn == '-':
+        # eg fiberassign-081000.fits.gz
+        scndfn = ''
     if len(scndfn):
-        scnd = read_secondary(scndfn)
+        print('Reading secondary targets file:', scndfn)
+        scnd,sidmap = read_secondary(scndfn)
         print('Read', len(scnd), 'secondaries from', scndfn)
-        sidmap = dict([(tid,i) for i,tid in enumerate(scnd['TARGETID']) if tid > 0])
 
         # Match on targetid
         I = np.array([sidmap.get(t, -1) for t in targetid])
@@ -157,6 +200,12 @@ def patch(infn = 'desi/target/fiberassign/tiles/trunk/001/fiberassign-001000.fit
     
         for col in ['FLUX_G', 'FLUX_R', 'FLUX_Z',
                     'GAIA_PHOT_G_MEAN_MAG', 'GAIA_PHOT_BP_MEAN_MAG', 'GAIA_PHOT_RP_MEAN_MAG',]:
+            # if not col in tab.dtype.fields:
+            #     print('No column', col, 'in original FIBERASSIGN table')
+            #     continue
+            if not col in scnd.dtype.fields:
+                print('No column', col, 'in secondary table')
+                continue
             old = tab[col][J]
             new = scnd[col][I]
             eq = equal(old, new)
@@ -170,18 +219,38 @@ def patch(infn = 'desi/target/fiberassign/tiles/trunk/001/fiberassign-001000.fit
                 tab[col][J[diff]] = new[diff]
                 patched_rows.update(J[diff])
 
-    # ToO files seem to have been moved... map old->new locations.
-    toomap = {
-        '/global/cfs/cdirs/desi/survey/ops/staging/mtl/main/ToO/ToO.ecsv' :
-        '/global/cfs/cdirs/desi/target/ToO/ToO.ecsv',
 
-        '/data/afternoon_planning/surveyops/trunk/mtl/sv3/ToO/ToO.ecsv' :
-        '/global/cfs/cdirs/desi/target/ToO/sv3/ToO.ecsv',
-        }
-    toofn = toomap.get(toofn, toofn)
+    # ToO files -- we're going to ignore the header values and look up the file location based on FA_SURV.
+    toomap = {
+        'sv3': '/global/cfs/cdirs/desi/survey/ops/surveyops/trunk/mtl/sv3/ToO/ToO.ecsv',
+        'main': '/global/cfs/cdirs/desi/survey/ops/surveyops/trunk/mtl/main/ToO/ToO.ecsv',
+        'cmx': '',
+        'sv2': '',
+        'sv1': '',
+    }
+
+    # # ToO files seem to have been moved... map old->new locations.
+    # toomap = {
+    #     '/global/cfs/cdirs/desi/survey/ops/staging/mtl/main/ToO/ToO.ecsv' :
+    #     '/global/cfs/cdirs/desi/target/ToO/ToO.ecsv',
+    # 
+    #     '/data/afternoon_planning/surveyops/trunk/mtl/sv3/ToO/ToO.ecsv' :
+    #     '/global/cfs/cdirs/desi/target/ToO/sv3/ToO.ecsv',
+    #}
+    #toofn = toomap.get(toofn, toofn)
+
+    if not fa_surv in toomap:
+        print('Failed to find ToO file for FA_SURV', fa_surv)
+
+    toofn = toomap[fa_surv]
+    #toofn = toomap.get(fa_surv, '')
+    #if len(toofn) == 0:
+    #    print('Did not find ToO file location for FA_SURV', fa_surv)
+    #    assert(False)
 
     # Were ToO targets used in this tile?
     if len(toofn):
+        print('Reading ToO file', toofn)
         too = Table.read(toofn)
         print('Read', len(too), 'ToO from', toofn)
         toomap = dict([(tid,i) for i,tid in enumerate(too['TARGETID']) if tid > 0])
