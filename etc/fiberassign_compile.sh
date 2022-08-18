e7122806c250c9fa6bfc217b492652f24d556bd0
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2022-08-18 15:10:21 -0700

    remove refernece to INSTALL_DIR

--- etc/fiberassign_compile.sh
+++ etc/fiberassign_compile.sh
@@ -6,12 +6,8 @@
 # The single command-line argument is the path to the Python executable
 # that is calling desiInstall.
 #
-# This is meant to be run in ${INSTALL_DIR}, which is automatically
-# defined by desiInstall.
-#
 # As of 2022, setup.py in generally deprecated, so whatever replaces it
 # can go in this script.
 #
 py=$1
-cd ${INSTALL_DIR}
 ${py} setup.py build_ext --inplace
