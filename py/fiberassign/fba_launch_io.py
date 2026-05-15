52bb475041004f58d5147fe8380fb0591a6b7cbe
dylanagreen <dylanag@uci.edu>
2026-05-15 09:23:38 -0700

    Fix harmless bug where --ha is duplicated in fba_run call.

--- py/fiberassign/fba_launch_io.py
+++ py/fiberassign/fba_launch_io.py
@@ -2129,8 +2129,6 @@ def launch_onetile_fa(
         "--ha",
         str(args.ha),
     ]
-    if args.ha != 0:
-        opts += ["--ha", str(args.ha)]
     if args.margin_pos != 0:
         opts += ["--margin-pos", str(args.margin_pos)]
     if args.margin_gfa != 0:
